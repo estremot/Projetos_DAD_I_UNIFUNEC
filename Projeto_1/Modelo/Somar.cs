be7e9bdbea0dbe7dcd3234006de638d397d940e5
Marcos Antonio Estremote <52921427+estremot@users.noreply.github.com>
2023-02-03 21:54:50 -0300

    Projeto 1 03_02
    Conclusão do Projeto 1

---
+++ Projeto_1/Modelo/Somar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_1.Modelo
+{
+    public class Somar
+    {
+        private int Valor1 { get; set; }
+        private int Valor2 { get; set; }
+        private int Total { get; set; }
+
+        public int Calcular()
+        {
+            this.Total = this.Valor1 + this.Valor2;
+            return this.Total;
+        }
+
+        public Somar(int Valor1, int Valor2)
+        {
+            this.Valor1 = Valor1;
+            this.Valor2 = Valor2;
+        }
+
+      
+    }
+}
---
+++ Projeto_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Projeto_1.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto 1 - C#");
+
+            //Comentário de 1 linha
+
+            /*
+             * comentário de Várias
+             * Linhas
+             */
+
+            Console.WriteLine("Informe seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Seu nome é:" + nome);
+
+            Console.WriteLine($"Seu nome é: {nome}");
+
+            Console.WriteLine("SOMANDO 2 VALORES!!!");
+
+            Console.WriteLine("Informe o Primeiro Valor: ");
+            int valor1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o Segundo Valor: ");
+            int valor2 = Int32.Parse(Console.ReadLine());   
+
+
+            int total = valor1+ valor2;
+
+            Console.WriteLine($"Total: {total}");
+
+            Somar sm = new Somar(valor1,valor2);
+            Console.WriteLine($"Total: {sm.Calcular()}");
+            
+
+            Console.ReadLine();
+        }
+    }
+}
