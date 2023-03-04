51cd5e7dba8d886a610cd95148385e678ea15a91
Marcos Antonio Estremote <52921427+estremot@users.noreply.github.com>
2023-03-03 22:02:32 -0300

    Projeto_7
    Exemplo de Componentes

---
+++ Projeto_7/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Projeto_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.txt_mascara = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox5 = new System.Windows.Forms.MaskedTextBox();
+            this.chb_mostrar = new System.Windows.Forms.CheckBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.datas = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_cidades = new System.Windows.Forms.TextBox();
+            this.txt_cidade = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(12, 37);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 0;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(12, 79);
+            this.maskedTextBox2.Mask = "(99) 0000-0000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox2.TabIndex = 1;
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(12, 124);
+            this.maskedTextBox3.Mask = "000.000.000-00";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox3.TabIndex = 2;
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(173, 37);
+            this.maskedTextBox4.Mask = "$ 999,999.00";
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(189, 20);
+            this.maskedTextBox4.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(173, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Máscaras";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(45, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "CPF";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Click += new System.EventHandler(this.radioButton3_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "Celular";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(79, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Data Nasc.";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // txt_mascara
+            // 
+            this.txt_mascara.Location = new System.Drawing.Point(173, 195);
+            this.txt_mascara.Mask = "00/00/0000";
+            this.txt_mascara.Name = "txt_mascara";
+            this.txt_mascara.Size = new System.Drawing.Size(200, 20);
+            this.txt_mascara.TabIndex = 5;
+            this.txt_mascara.ValidatingType = typeof(System.DateTime);
+            // 
+            // maskedTextBox5
+            // 
+            this.maskedTextBox5.Location = new System.Drawing.Point(401, 37);
+            this.maskedTextBox5.Name = "maskedTextBox5";
+            this.maskedTextBox5.PasswordChar = '*';
+            this.maskedTextBox5.Size = new System.Drawing.Size(145, 20);
+            this.maskedTextBox5.TabIndex = 6;
+            // 
+            // chb_mostrar
+            // 
+            this.chb_mostrar.AutoSize = true;
+            this.chb_mostrar.Location = new System.Drawing.Point(552, 40);
+            this.chb_mostrar.Name = "chb_mostrar";
+            this.chb_mostrar.Size = new System.Drawing.Size(61, 17);
+            this.chb_mostrar.TabIndex = 7;
+            this.chb_mostrar.Text = "Mostrar";
+            this.chb_mostrar.UseVisualStyleBackColor = true;
+            this.chb_mostrar.Click += new System.EventHandler(this.chb_mostrar_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(401, 69);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 8;
+            // 
+            // datas
+            // 
+            this.datas.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.datas.Location = new System.Drawing.Point(641, 34);
+            this.datas.Name = "datas";
+            this.datas.Size = new System.Drawing.Size(200, 20);
+            this.datas.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(847, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(806, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txt_cidades
+            // 
+            this.txt_cidades.Location = new System.Drawing.Point(640, 85);
+            this.txt_cidades.Multiline = true;
+            this.txt_cidades.Name = "txt_cidades";
+            this.txt_cidades.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_cidades.Size = new System.Drawing.Size(214, 117);
+            this.txt_cidades.TabIndex = 12;
+            this.txt_cidades.Text = "ILHA SOLTEIRA\r\nSANTA FÉ DO SUL\r\nSANTA SALETE";
+            // 
+            // txt_cidade
+            // 
+            this.txt_cidade.Location = new System.Drawing.Point(641, 8);
+            this.txt_cidade.Name = "txt_cidade";
+            this.txt_cidade.Size = new System.Drawing.Size(187, 20);
+            this.txt_cidade.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 255);
+            this.Controls.Add(this.txt_cidade);
+            this.Controls.Add(this.txt_cidades);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.datas);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.chb_mostrar);
+            this.Controls.Add(this.maskedTextBox5);
+            this.Controls.Add(this.txt_mascara);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.maskedTextBox4);
+            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.MaskedTextBox txt_mascara;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox5;
+        private System.Windows.Forms.CheckBox chb_mostrar;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.DateTimePicker datas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_cidades;
+        private System.Windows.Forms.TextBox txt_cidade;
+    }
+}
+
---
+++ Projeto_7/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            txt_mascara.Text = "";
+            txt_mascara.Mask = "00/00/0000";
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            txt_mascara.Text = "";
+            txt_mascara.Mask = "(00) 00000-0000";
+        }
+
+        private void radioButton3_Click(object sender, EventArgs e)
+        {
+
+            txt_mascara.Text = "";
+            txt_mascara.Mask = "000.000.000-00";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chb_mostrar_Click(object sender, EventArgs e)
+        {
+            if(chb_mostrar.Checked)
+            {
+                maskedTextBox5.PasswordChar = '\0';
+            }
+            else
+            {
+                maskedTextBox5.PasswordChar = '*';
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
