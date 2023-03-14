e74dee1756f8d74edc5a4d90b203a4f3153ff2a4
Marcos Antonio Estremote <52921427+estremot@users.noreply.github.com>
2023-03-14 20:18:55 -0300

    Projeto Animação
    Segue o Projeto

---
+++ SimulaEnter/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SimulaEnter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.Image = global::SimulaEnter.Properties.Resources.img1;
+            this.label3.Location = new System.Drawing.Point(257, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 46);
+            this.label3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 211);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SimulaEnter/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SimulaEnter
+{
+    public partial class Form1 : Form
+    {
+
+        int valor = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            valor++;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           // label1.Text = ((int)(e.KeyChar))+"";
+           if(e.KeyChar == 27)
+            {
+                //Application.Exit();
+            }
+            if(e.KeyChar == 13)
+            {
+                MessageBox.Show("Você apertou o Enter!!!");
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8 && e.KeyChar != 44)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            System.Drawing.Image image1 = System.Drawing.Image.FromFile("C:\\Users\\marco\\Desktop\\img1.png");
+            System.Drawing.Image image2 = System.Drawing.Image.FromFile("C:\\Users\\marco\\Desktop\\img2.png");
+
+            // Set the size of the label to accommodate the bitmap size.
+
+            label1.Size = new Size(image1.Width, image1.Height);
+
+
+            if (valor % 2 == 0)
+            {
+                label3.Image = image1;
+            }
+            else
+            {
+                label3.Image = image2;
+            }
+            valor++;
+
+            if(e.KeyCode == Keys.Up)
+            {
+                label3.Location = new Point(label3.Location.X, label3.Location.Y - 5);
+                label3.BackColor = Color.Red;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                label3.Location = new Point(label3.Location.X, label3.Location.Y + 5);
+                label3.BackColor = Color.Red;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                label3.Location = new Point(label3.Location.X - 5, label3.Location.Y);
+                label3.BackColor = Color.Blue;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                label3.Location = new Point(label3.Location.X + 5, label3.Location.Y);
+                label3.BackColor = Color.Blue;
+            }
+        }
+    }
+}
